e7ea973c2b6b68b82800fe31f18df7cb46183c3a
jinzitian <421833081@qq.com>
2017-03-11 13:10:53 +0800

    .

--- main.py
+++ main.py
@@ -21,18 +21,25 @@ def get_data():
     
     
 def main(args):
-
+    
     if args[1] == 'train':
         train()
     elif args[1] == 'example':
         data = get_data()
         sentence = [data['user_content'].iloc[np.random.randint(len(data))]]
-        for s, e in predict(sentence):
-            print(s, '[' + e + ']')
+        try:
+            for s, e in predict(sentence):
+                print(s, '[' + e + ']')
+        except Exception as e:
+            print('you need train first')
+            
     elif args[1] == 'sentence':
         sentence = args[2:]
-        for s,e in predict(sentence):
-            print(s, '[' + e + ']')
+        try:
+            for s,e in predict(sentence):
+                print(s, '[' + e + ']')
+        except Exception as e:
+            print('you need train first')
     else:
         print('you can try like these:')
         print('  1、python main.py train')
@@ -42,3 +49,8 @@ def main(args):
 if __name__ == '__main__':
     
     main(sys.argv)
+    
+    
+    
+    
+    
